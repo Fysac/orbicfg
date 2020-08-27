d86fe19d8bdbda67ad82cf2c480588cea6f2f98e
Fysac <Fysac@users.noreply.github.com>
2020-08-27 19:26:42 -0400

    Validate checksum

--- main.c
+++ main.c
@@ -12,11 +12,15 @@
 // The real encrypted data is at this offset.
 #define WEB_CONFIG_OFFSET 0xa0000
 
-// Format: [magic (4 bytes), config length (4 bytes), crc (4 bytes)]
-// Values are stored little-endian.
-#define HEADER_SIZE 12
-
 #define MAX_HEADER_AND_CONFIG_SIZE 0x39000
+
+typedef struct config_header {
+    uint32_t magic;
+    uint32_t len;
+    uint32_t crc;
+} config_header;
+
+int validate_checksum(config_header *config);
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -36,8 +40,6 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    memset(buf, 0, MAX_HEADER_AND_CONFIG_SIZE);
-
     fread(buf, strlen(WEB_CONFIG_TAR_NAME), 1, f);
     if (!strncmp((char *) buf, WEB_CONFIG_TAR_NAME, strlen(WEB_CONFIG_TAR_NAME))) {
         if (fseek(f, WEB_CONFIG_OFFSET, SEEK_SET) < 0) {
@@ -47,28 +49,32 @@ int main(int argc, char *argv[]) {
     } else {
         rewind(f);
     }
-
     fread(buf, MAX_HEADER_AND_CONFIG_SIZE, 1, f);
     fclose(f);
 
-    uint32_t config_len = ((uint32_t *)buf)[1];
-    assert(HEADER_SIZE + config_len <= MAX_HEADER_AND_CONFIG_SIZE);
-    assert(config_len > 0 && config_len % sizeof(uint32_t) == 0);
+    config_header *config = (config_header *) buf;
+    if (config->len == 0 || config->len + sizeof(config_header) > MAX_HEADER_AND_CONFIG_SIZE) {
+        fprintf(stderr, "invalid config length (0x%08x)\n", config->len);
+        exit(-1);
+    }
 
     // Seed given to uClibc srand() to generate XOR keystream.
     // Often 0x20131224 or 0x23091293.
-    uint32_t rand_magic = *((uint32_t *)buf);
-    uclibc_srandom(rand_magic);
+    uclibc_srandom(config->magic);
 
     // XOR every 4 bytes with the next call to uClibc rand().
-    uint32_t *p = (uint32_t *)(buf + HEADER_SIZE);
-    while (p < (uint32_t *)(buf + HEADER_SIZE + config_len)) {
+    uint32_t *p = (uint32_t *) (buf + sizeof(config_header));
+    while (p < (uint32_t *) (buf + config->len + sizeof(config_header))) {
         *p = *p ^ uclibc_random();
         p++;
     }
 
-    int i = 0;
-    for (i = HEADER_SIZE; i < config_len + HEADER_SIZE; i++) {
+    if (!validate_checksum(config)) {
+        fprintf(stderr, "invalid checksum (0x%08x)\n", config->crc);
+        exit(-1);
+    }
+
+    for (int i = sizeof(config_header); i < config->len + sizeof(config_header); i++) {
         if (buf[i] == '\0') {
             printf("\n");
         } else {
@@ -78,4 +84,16 @@ int main(int argc, char *argv[]) {
 
     free(buf);
     return EXIT_SUCCESS;
+}
+
+int validate_checksum(config_header *config) {
+    uint32_t len = config->len / sizeof(uint32_t);
+    uint32_t crc = config->crc;
+    config++;
+
+    for (int i = 0; i < len; i++) {
+        crc += config->magic;
+        config = (config_header *) &config->len;
+    }
+    return crc == 0xffffffff;
 }
