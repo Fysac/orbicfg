66bca7d8dc7b35f2f1acc9e032aeb1cd544e7fdf
Fysac <Fysac@users.noreply.github.com>
2024-10-20 22:53:46 +0000

    Add override for RAX10 (#8)

--- cfg/overrides.go
+++ cfg/overrides.go
@@ -12,6 +12,16 @@ var overrides = map[uint32]*Metadata{
 		RealMagic:    0x01346232,
 		Rng:          RngMusl,
 	},
+	// RAX10 (https://github.com/Fysac/orbicfg/issues/8)
+	0x20200425: {
+		HeaderOffset: 0,
+		StatedMagic:  0x20200425,
+		RealMagic:    0x20200426,
+		/* Technically, the RAX10 uses glibc. However, the version in use appears to behave identically to
+		 * the uClibc implementation we already have, so we'll just use that.
+		 * TODO: revisit this */
+		Rng: RngUclibc,
+	},
 }
 
 func Overrides() map[uint32]*Metadata {
